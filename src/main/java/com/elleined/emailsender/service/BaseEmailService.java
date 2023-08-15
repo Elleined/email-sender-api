15f7377a70873ff28645e8b92cb2933a65d55c14
Elleined <demadegu@gmail.com>
2023-08-15 20:45:01 +0800

    added base email service

--- src/main/java/com/elleined/emailsender/service/BaseEmailService.java
+++ src/main/java/com/elleined/emailsender/service/BaseEmailService.java
@@ -1,11 +1,12 @@
 package com.elleined.emailsender.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.javamail.JavaMailSender;
  abstract class BaseEmailService {
+     @Autowired
+     protected JavaMailSender javaMailSender;
 
-    protected JavaMailSender javaMailSender;
-
-    @Value("spring.mail.username")
-    protected String sender;
+     @Value("${spring.mail.username}")
+     protected String sender;
 }

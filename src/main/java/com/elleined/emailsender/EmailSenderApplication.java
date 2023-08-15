98d89c5fefb400f3efdcc6b794bcdb7e73c27da8
Elleined <demadegu@gmail.com>
2023-08-15 20:17:02 +0800

    added spring security for bcrypt

--- src/main/java/com/elleined/emailsender/EmailSenderApplication.java
+++ src/main/java/com/elleined/emailsender/EmailSenderApplication.java
@@ -2,12 +2,16 @@ package com.elleined.emailsender;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@EnableDiscoveryClient
+@EnableTransactionManagement
 public class EmailSenderApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmailSenderApplication.class, args);
 	}
-
 }

e4093640154154d9b316fde513e30b484e336f84
Elleined <demadegu@gmail.com>
2025-05-19 21:44:34 +0800

    Removed APIResponse

--- src/main/java/com/elleined/emailsenderapi/EmailSenderApiApplication.java
+++ src/main/java/com/elleined/emailsenderapi/EmailSenderApiApplication.java
@@ -2,9 +2,6 @@ package com.elleined.emailsenderapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import java.security.SecureRandom;
 
 @SpringBootApplication
 public class EmailSenderApiApplication {
--- src/main/java/com/elleined/emailsenderapi/controller/ExceptionController.java
+++ src/main/java/com/elleined/emailsenderapi/controller/ExceptionController.java
@@ -1,6 +1,5 @@
 package com.elleined.emailsenderapi.controller;
 
-import com.elleined.emailsenderapi.request.APIResponse;
 import jakarta.mail.MessagingException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,23 +15,21 @@ import java.util.List;
 public class ExceptionController {
 
     @ExceptionHandler(BindException.class)
-    public ResponseEntity<List<APIResponse>> handleBindException(BindException ex) {
-        List<APIResponse> errors = ex.getBindingResult().getAllErrors().stream()
+    public ResponseEntity<List<String>> handleBindException(BindException ex) {
+        List<String> errors = ex.getBindingResult().getAllErrors().stream()
                 .map(ObjectError::getDefaultMessage)
-                .map(errorMessage -> new APIResponse(HttpStatus.BAD_REQUEST, errorMessage))
                 .toList();
+
         return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(MessagingException.class)
-    public ResponseEntity<APIResponse> handleMessagingException(MessagingException ex) {
-        var responseMessage = new APIResponse(HttpStatus.SERVICE_UNAVAILABLE, "Cannot send email! Probably theres something wrong with the mail service! " + ex.getMessage());
-        return new ResponseEntity<>(responseMessage, HttpStatus.SERVICE_UNAVAILABLE);
+    public ResponseEntity<String> handleMessagingException(MessagingException ex) {
+        return new ResponseEntity<>( "Cannot send email! Probably theres something wrong with the mail service! " + ex.getMessage(), HttpStatus.SERVICE_UNAVAILABLE);
     }
 
     @ExceptionHandler(IOException.class)
-    public ResponseEntity<APIResponse> handleIOException(IOException ex) {
-        var responseMessage = new APIResponse(HttpStatus.NOT_ACCEPTABLE, "Cannot send email with attachement! " + ex.getMessage());
-        return new ResponseEntity<>(responseMessage, HttpStatus.NOT_ACCEPTABLE);
+    public ResponseEntity<String> handleIOException(IOException ex) {
+        return new ResponseEntity<>("Cannot send email with attachment! " + ex.getMessage(), HttpStatus.NOT_ACCEPTABLE);
     }
 }

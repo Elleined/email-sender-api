fd345f3dde5877fcf76e7ce25bf4c94b15a06880
Elleined <demadegu@gmail.com>
2025-06-01 19:38:24 +0800

    Added deployment folder

--- src/main/java/com/elleined/emailsenderapi/OTPController.java
+++ src/main/java/com/elleined/emailsenderapi/OTPController.java
@@ -4,7 +4,6 @@ import jakarta.mail.MessagingException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.security.SecureRandom;

da2f6f0c00f38e97b1d46bc700c804608cee9389
elleined <demadegu@gmail.com>
2025-08-29 15:37:57 +0800

    - Remove regexp in @Email - Upgraded to Spring Boot 3.5.5 - Remove unused imports

--- src/main/java/com/elleined/emailsenderapi/MailService.java
+++ src/main/java/com/elleined/emailsenderapi/MailService.java
@@ -3,14 +3,13 @@ package com.elleined.emailsenderapi;
 import jakarta.mail.MessagingException;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 
 public interface MailService {
-    void send(@NotBlank @Email(regexp = "^[\\w!#$%&'*+/=?`{|}~^-]+(?:\\.[\\w!#$%&'*+/=?`{|}~^-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$") String receiver,
+    void send(@NotBlank @Email String receiver,
               @NotBlank String subject,
               @NotBlank String message) throws MessagingException;
 
-    void send(@NotBlank @Email(regexp = "^[\\w!#$%&'*+/=?`{|}~^-]+(?:\\.[\\w!#$%&'*+/=?`{|}~^-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$") String receiver,
+    void send(@NotBlank @Email String receiver,
               @NotBlank String subject,
               @NotBlank String message,
               @NotBlank String fileName,
